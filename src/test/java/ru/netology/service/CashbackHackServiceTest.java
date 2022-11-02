14b9f33cc5ba19a62de7bc192a1ae26b4ea2dbd3
Регина Васильева <skafest123@gmail.com>
2022-11-02 18:09:11 +0700

    v2

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
@@ -15,22 +14,21 @@ public class CashbackHackServiceTest {
         Assert.assertEquals(actual, expected);
 
     }
-
     @Test
     public void testRemainAdd0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
-
     @Test
     public void testRemainAdd1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int expected = 1000;
         int actual = service.remain(amount);
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
+
 }
